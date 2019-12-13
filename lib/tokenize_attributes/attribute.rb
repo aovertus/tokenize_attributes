20924fab11241f2ea784b6679e209625ca7e8840
Alex O <al3x_over@hotmail.com>
2019-12-13 11:28:36 -0500

    test

--- lib/tokenize_attributes/attribute.rb
+++ lib/tokenize_attributes/attribute.rb
@@ -7,16 +7,17 @@ module TokenizeAttributes
     end
 
     def tokenized_attribute(attrb, method = nil)
-      self.send(:define_method, "reset_tokenization_for_#{attrb}") do
-        loop do
+      self.send(:define_method, 'reset_tokenization_for_#{attrb}') do
+        loop {
           token = method.try(:call) || TokenizeAttributes.configuration.tokenizer.call
           self.public_send("#{attrb}=", token)
-        break unless self.class.where(attrb => token).exists?
-        end
+          break unless self.class.where(attrb => token).exists?
+        }
       end
 
       before_validation do |record|
-        record.public_send("reset_tokenization_for_#{attrb}") if record.public_send(attrb).blank?
+        return unless record.public_send(attrb).blank?
+        record.public_send("reset_tokenization_for_#{attrb}")
       end
     end
   end
